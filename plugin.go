bc040ac5fbb74c6937c0da5ee47fd6e016a7945e
David Wallach <davidwallach2@gmail.com>
2022-10-04 12:29:46 -0400

    wait time to avoid ResourceConflictException

--- plugin.go
+++ plugin.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -251,6 +252,25 @@ func (p Plugin) Exec() error {
 				log.Println(err.Error())
 			}
 			return err
+		}
+
+		if p.Config.Debug {
+			log.Println(result)
+		}
+
+		for {
+			getCfg := &lambda.GetFunctionConfigurationInput{}
+			getCfg.SetFunctionName(p.Config.FunctionName)
+
+			result, err := svc.GetFunctionConfiguration(getCfg)
+			if err != nil {
+				return err
+			}
+			if *result.State == "Pending" {
+				time.Sleep(1 * time.Second)
+			} else {
+				break
+			}
 		}
 
 		if p.Config.Debug {
