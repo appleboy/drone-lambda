2f22aebf984c58cf6af8dd67c47f548a28286638
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-04-01 15:46:45 +0800

    chore: update indirect dependencies for improved functionality
    - Update `urfave/cli` to version `v2.25.1`
    - Add `davecgh/go-spew` as an indirect dependency
    - Add `xrash/smetrics` and `yaml.v2` as indirect dependencies
    
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- go.mod
+++ go.mod
@@ -6,12 +6,13 @@ require (
 	github.com/aws/aws-sdk-go v1.44.233
 	github.com/joho/godotenv v1.5.1
 	github.com/mholt/archiver/v3 v3.5.1
-	github.com/urfave/cli v1.22.12
+	github.com/urfave/cli/v2 v2.25.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
@@ -22,4 +23,6 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 // Version set at compile-time
@@ -24,7 +24,7 @@ func main() {
 	app.Name = "Drone Lambda"
 	app.Usage = "Deploying Lambda code with drone CI to an existing function"
 	app.Copyright = "Copyright (c) 2020 Bo-Yi Wu"
-	app.Authors = []cli.Author{
+	app.Authors = []*cli.Author{
 		{
 			Name:  "Bo-Yi Wu",
 			Email: "appleboy.tw@gmail.com",
@@ -33,146 +33,147 @@ func main() {
 	app.Action = run
 	app.Version = Version
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "region",
-			Usage:  "AWS Region",
-			EnvVar: "PLUGIN_REGION,PLUGIN_AWS_REGION,AWS_REGION,INPUT_AWS_REGION",
-			Value:  "us-east-1",
+		&cli.StringFlag{
+			Name:    "region",
+			Usage:   "AWS Region",
+			EnvVars: []string{"PLUGIN_REGION", "PLUGIN_AWS_REGION", "AWS_REGION", "INPUT_AWS_REGION"},
+			Value:   "us-east-1",
 		},
-		cli.StringFlag{
-			Name:   "access-key",
-			Usage:  "AWS ACCESS KEY",
-			EnvVar: "PLUGIN_ACCESS_KEY,PLUGIN_AWS_ACCESS_KEY_ID,AWS_ACCESS_KEY_ID,INPUT_AWS_ACCESS_KEY_ID",
+		&cli.StringFlag{
+			Name:    "access-key",
+			Usage:   "AWS ACCESS KEY",
+			EnvVars: []string{"PLUGIN_ACCESS_KEY", "PLUGIN_AWS_ACCESS_KEY_ID", "AWS_ACCESS_KEY_ID", "INPUT_AWS_ACCESS_KEY_ID"},
 		},
-		cli.StringFlag{
-			Name:   "secret-key",
-			Usage:  "AWS SECRET KEY",
-			EnvVar: "PLUGIN_SECRET_KEY,PLUGIN_AWS_SECRET_ACCESS_KEY,AWS_SECRET_ACCESS_KEY,INPUT_AWS_SECRET_ACCESS_KEY",
+		&cli.StringFlag{
+			Name:    "secret-key",
+			Usage:   "AWS SECRET KEY",
+			EnvVars: []string{"PLUGIN_SECRET_KEY", "PLUGIN_AWS_SECRET_ACCESS_KEY", "AWS_SECRET_ACCESS_KEY", "INPUT_AWS_SECRET_ACCESS_KEY"},
 		},
-		cli.StringFlag{
-			Name:   "session-token",
-			Usage:  "AWS Session token",
-			EnvVar: "PLUGIN_SESSION_TOKEN,PLUGIN_AWS_SESSION_TOKEN,AWS_SESSION_TOKEN,INPUT_AWS_SESSION_TOKEN",
+		&cli.StringFlag{
+			Name:    "session-token",
+			Usage:   "AWS Session token",
+			EnvVars: []string{"PLUGIN_SESSION_TOKEN", "PLUGIN_AWS_SESSION_TOKEN", "AWS_SESSION_TOKEN", "INPUT_AWS_SESSION_TOKEN"},
 		},
-		cli.StringFlag{
-			Name:   "aws-profile",
-			Usage:  "AWS profile",
-			EnvVar: "PLUGIN_PROFILE,PLUGIN_AWS_PROFILE,AWS_PROFILE,INPUT_AWS_PROFILE",
+		&cli.StringFlag{
+			Name:    "aws-profile",
+			Usage:   "AWS profile",
+			EnvVars: []string{"PLUGIN_PROFILE", "PLUGIN_AWS_PROFILE", "AWS_PROFILE", "INPUT_AWS_PROFILE"},
 		},
-		cli.StringFlag{
-			Name:   "function-name",
-			Usage:  "AWS lambda function name",
-			EnvVar: "PLUGIN_FUNCTION_NAME,FUNCTION_NAME,INPUT_FUNCTION_NAME",
+		&cli.StringFlag{
+			Name:    "function-name",
+			Usage:   "AWS lambda function name",
+			EnvVars: []string{"PLUGIN_FUNCTION_NAME", "FUNCTION_NAME", "INPUT_FUNCTION_NAME"},
 		},
-		cli.StringFlag{
-			Name:   "reversion-id",
-			Usage:  "Only update the function if the revision ID matches the ID that's specified.",
-			EnvVar: "PLUGIN_REVERSION_ID,REVERSION_ID,INPUT_REVERSION_ID",
+		&cli.StringFlag{
+			Name:    "reversion-id",
+			Usage:   "Only update the function if the revision ID matches the ID that's specified.",
+			EnvVars: []string{"PLUGIN_REVERSION_ID", "REVERSION_ID", "INPUT_REVERSION_ID"},
 		},
-		cli.StringFlag{
-			Name:   "s3-bucket",
-			Usage:  "An Amazon S3 bucket in the same AWS Region as your function. The bucket can be in a different AWS account.",
-			EnvVar: "PLUGIN_S3_BUCKET,S3_BUCKET,INPUT_S3_BUCKET",
+		&cli.StringFlag{
+			Name:    "s3-bucket",
+			Usage:   "An Amazon S3 bucket in the same AWS Region as your function. The bucket can be in a different AWS account.",
+			EnvVars: []string{"PLUGIN_S3_BUCKET", "S3_BUCKET", "INPUT_S3_BUCKET"},
 		},
-		cli.StringFlag{
-			Name:   "s3-key",
-			Usage:  "The Amazon S3 key of the deployment package.",
-			EnvVar: "PLUGIN_S3_KEY,S3_KEY,INPUT_S3_KEY",
+		&cli.StringFlag{
+			Name:    "s3-key",
+			Usage:   "The Amazon S3 key of the deployment package.",
+			EnvVars: []string{"PLUGIN_S3_KEY", "S3_KEY", "INPUT_S3_KEY"},
 		},
-		cli.StringFlag{
-			Name:   "s3-object-version",
-			Usage:  "AWS lambda s3 object version",
-			EnvVar: "PLUGIN_S3_OBJECT_VERSION,S3_OBJECT_VERSION,INPUT_S3_OBJECT_VERSION",
+		&cli.StringFlag{
+			Name:    "s3-object-version",
+			Usage:   "AWS lambda s3 object version",
+			EnvVars: []string{"PLUGIN_S3_OBJECT_VERSION", "S3_OBJECT_VERSION", "INPUT_S3_OBJECT_VERSION"},
 		},
-		cli.StringFlag{
-			Name:   "zip-file",
-			Usage:  "AWS lambda zip file",
-			EnvVar: "PLUGIN_ZIP_FILE,ZIP_FILE,INPUT_ZIP_FILE",
+		&cli.StringFlag{
+			Name:    "zip-file",
+			Usage:   "AWS lambda zip file",
+			EnvVars: []string{"PLUGIN_ZIP_FILE", "ZIP_FILE", "INPUT_ZIP_FILE"},
 		},
-		cli.StringSliceFlag{
-			Name:   "source",
-			Usage:  "zip file list",
-			EnvVar: "PLUGIN_SOURCE,SOURCE,INPUT_SOURCE",
+		&cli.StringSliceFlag{
+			Name:    "source",
+			Usage:   "zip file list",
+			EnvVars: []string{"PLUGIN_SOURCE", "SOURCE", "INPUT_SOURCE"},
 		},
-		cli.BoolFlag{
-			Name:   "dry-run",
-			Usage:  "Set to true to validate the request parameters and access permissions without modifying the function code.",
-			EnvVar: "PLUGIN_DRY_RUN,DRY_RUN,INPUT_DRY_RUN",
+		&cli.BoolFlag{
+			Name:    "dry-run",
+			Usage:   "Set to true to validate the request parameters and access permissions without modifying the function code.",
+			EnvVars: []string{"PLUGIN_DRY_RUN", "DRY_RUN", "INPUT_DRY_RUN"},
 		},
-		cli.BoolFlag{
-			Name:   "debug",
-			Usage:  "Show debug message after upload the lambda successfully.",
-			EnvVar: "PLUGIN_DEBUG,DEBUG,INPUT_DEBUG",
+		&cli.BoolFlag{
+			Name:    "debug",
+			Usage:   "Show debug message after upload the lambda successfully.",
+			EnvVars: []string{"PLUGIN_DEBUG", "DEBUG", "INPUT_DEBUG"},
 		},
-		cli.BoolTFlag{
-			Name:   "publish",
-			Usage:  "Set to true to publish a new version of the function after updating the code.",
-			EnvVar: "PLUGIN_PUBLISH,PUBLISH,INPUT_PUBLISH",
+		&cli.BoolFlag{
+			Name:    "publish",
+			Usage:   "Set to true to publish a new version of the function after updating the code.",
+			EnvVars: []string{"PLUGIN_PUBLISH", "PUBLISH", "INPUT_PUBLISH"},
+			Value:   true,
 		},
-		cli.Int64Flag{
-			Name:   "memory-size",
-			Usage:  "The amount of memory that your function has access to. Increasing the function's memory also increases its CPU allocation. The default value is 128 MB. The value must be a multiple of 64 MB.",
-			EnvVar: "PLUGIN_MEMORY_SIZE,MEMORY_SIZE,INPUT_MEMORY_SIZE",
+		&cli.Int64Flag{
+			Name:    "memory-size",
+			Usage:   "The amount of memory that your function has access to. Increasing the function's memory also increases its CPU allocation. The default value is 128 MB. The value must be a multiple of 64 MB.",
+			EnvVars: []string{"PLUGIN_MEMORY_SIZE", "MEMORY_SIZE", "INPUT_MEMORY_SIZE"},
 		},
-		cli.Int64Flag{
-			Name:   "timeout",
-			Usage:  "The amount of time that Lambda allows a function to run before stopping it. The default is 3 seconds. The maximum allowed value is 900 seconds.",
-			EnvVar: "PLUGIN_TIMEOUT,TIMEOUT,INPUT_TIMEOUT",
+		&cli.Int64Flag{
+			Name:    "timeout",
+			Usage:   "The amount of time that Lambda allows a function to run before stopping it. The default is 3 seconds. The maximum allowed value is 900 seconds.",
+			EnvVars: []string{"PLUGIN_TIMEOUT", "TIMEOUT", "INPUT_TIMEOUT"},
 		},
-		cli.StringFlag{
-			Name:   "handler",
-			Usage:  "The name of the method within your code that Lambda calls to execute your function.",
-			EnvVar: "PLUGIN_HANDLER,HANDLER,INPUT_HANDLER",
+		&cli.StringFlag{
+			Name:    "handler",
+			Usage:   "The name of the method within your code that Lambda calls to execute your function.",
+			EnvVars: []string{"PLUGIN_HANDLER", "HANDLER", "INPUT_HANDLER"},
 		},
-		cli.StringFlag{
-			Name:   "role",
-			Usage:  "The Amazon Resource Name (ARN) of the function's execution role.",
-			EnvVar: "PLUGIN_ROLE,ROLE,INPUT_ROLE",
+		&cli.StringFlag{
+			Name:    "role",
+			Usage:   "The Amazon Resource Name (ARN) of the function's execution role.",
+			EnvVars: []string{"PLUGIN_ROLE", "ROLE", "INPUT_ROLE"},
 		},
-		cli.StringFlag{
-			Name:   "runtime",
-			Usage:  "The identifier of the function's runtime.",
-			EnvVar: "PLUGIN_RUNTIME,RUNTIME,INPUT_RUNTIME",
+		&cli.StringFlag{
+			Name:    "runtime",
+			Usage:   "The identifier of the function's runtime.",
+			EnvVars: []string{"PLUGIN_RUNTIME", "RUNTIME", "INPUT_RUNTIME"},
 		},
-		cli.StringSliceFlag{
-			Name:   "environment",
-			Usage:  "Lambda Environment variables",
-			EnvVar: "PLUGIN_ENVIRONMENT,ENVIRONMENT,INPUT_ENVIRONMENT",
+		&cli.StringSliceFlag{
+			Name:    "environment",
+			Usage:   "Lambda Environment variables",
+			EnvVars: []string{"PLUGIN_ENVIRONMENT", "ENVIRONMENT", "INPUT_ENVIRONMENT"},
 		},
-		cli.StringSliceFlag{
-			Name:   "layers",
-			Usage:  "A list of function layers",
-			EnvVar: "PLUGIN_LAYERS,LAYERS,INPUT_LAYERS",
+		&cli.StringSliceFlag{
+			Name:    "layers",
+			Usage:   "A list of function layers",
+			EnvVars: []string{"PLUGIN_LAYERS", "LAYERS", "INPUT_LAYERS"},
 		},
-		cli.StringFlag{
-			Name:   "commit.sha",
-			Usage:  "git commit sha",
-			EnvVar: "DRONE_COMMIT_SHA,GITHUB_SHA",
+		&cli.StringFlag{
+			Name:    "commit.sha",
+			Usage:   "git commit sha",
+			EnvVars: []string{"DRONE_COMMIT_SHA", "GITHUB_SHA"},
 		},
-		cli.StringFlag{
-			Name:   "commit.author",
-			Usage:  "git author name",
-			EnvVar: "DRONE_COMMIT_AUTHOR",
+		&cli.StringFlag{
+			Name:    "commit.author",
+			Usage:   "git author name",
+			EnvVars: []string{"DRONE_COMMIT_AUTHOR"},
 		},
-		cli.StringFlag{
-			Name:   "image-uri",
-			Usage:  "URI of a container image in the Amazon ECR registry.",
-			EnvVar: "PLUGIN_IMAGE_URI,IMAGE_URI,INPUT_IMAGE_URI",
+		&cli.StringFlag{
+			Name:    "image-uri",
+			Usage:   "URI of a container image in the Amazon ECR registry.",
+			EnvVars: []string{"PLUGIN_IMAGE_URI", "IMAGE_URI", "INPUT_IMAGE_URI"},
 		},
-		cli.StringSliceFlag{
-			Name:   "subnets",
-			Usage:  "Select the VPC subnets for Lambda to use to set up your VPC configuration.",
-			EnvVar: "PLUGIN_SUBNETS,SUBNETS,INPUT_SUBNETS",
+		&cli.StringSliceFlag{
+			Name:    "subnets",
+			Usage:   "Select the VPC subnets for Lambda to use to set up your VPC configuration.",
+			EnvVars: []string{"PLUGIN_SUBNETS", "SUBNETS", "INPUT_SUBNETS"},
 		},
-		cli.StringSliceFlag{
-			Name:   "securitygroups",
-			Usage:  "Choose the VPC security groups for Lambda to use to set up your VPC configuration.",
-			EnvVar: "PLUGIN_SECURITY_GROUPS,SECURITY_GROUPS,INPUT_SECURITY_GROUPS",
+		&cli.StringSliceFlag{
+			Name:    "securitygroups",
+			Usage:   "Choose the VPC security groups for Lambda to use to set up your VPC configuration.",
+			EnvVars: []string{"PLUGIN_SECURITY_GROUPS", "SECURITY_GROUPS", "INPUT_SECURITY_GROUPS"},
 		},
-		cli.StringFlag{
-			Name:   "description",
-			Usage:  "A description of the function.",
-			EnvVar: "PLUGIN_DESCRIPTION,DESCRIPTION,INPUT_DESCRIPTION",
+		&cli.StringFlag{
+			Name:    "description",
+			Usage:   "A description of the function.",
+			EnvVars: []string{"PLUGIN_DESCRIPTION", "DESCRIPTION", "INPUT_DESCRIPTION"},
 		},
 	}
 
